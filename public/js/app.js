de4693ad289368402d1a9a09f91df2000c590f85
Luis Fernando <luislecompt@gmail.com>
2021-05-11 15:25:04 -0500

    modificaciones

--- public/js/app.js
+++ public/js/app.js
@@ -52525,8 +52525,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/JoseSantiagoFigueroa/Documents/SistemasDistribuidos/MedicApp/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/JoseSantiagoFigueroa/Documents/SistemasDistribuidos/MedicApp/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Luis\Documents\Ing. Software\Octavo Semestre\Sistemas Distribuídos\Laravel-Vue-React\MedicApp\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Luis\Documents\Ing. Software\Octavo Semestre\Sistemas Distribuídos\Laravel-Vue-React\MedicApp\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
